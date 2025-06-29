acd59cbfef326386dd79711329452fc8df00bcce
Roshan Paudel <paudelroshan93@gmail.com>
2025-06-29 12:34:31 +1000

    Login page created and server get method defined

--- app.js
+++ app.js
@@ -9,10 +9,22 @@ const PORT = 8000;
 //Serve static file from public directory
 app.use(express.static(path.join(__dirname, "public")));
 
+//user home controller
 app.get("/", (req, res, next) => {
-  console.log("We got request");
+  console.log("Home page request");
   //sendFile method require complete file path
   //path.join used for cross compatility for path convention in different OS
+  res.sendFile(path.join(__dirname, "src/index.html"));
+});
+
+//user login controller
+app.get("/login", (req, res, next) => {
+  console.log("login request");
+  res.sendFile(path.join(__dirname, "src/login.html"));
+});
+//user register controller
+app.get("/register", (req, res, next) => {
+  console.log("Register request");
   res.sendFile(path.join(__dirname, "src/index.html"));
 });
 
@@ -27,5 +39,5 @@ app.get("/get-user", (req, res) => {
 app.listen(PORT, (error) => {
   error
     ? console.log(error)
-    : console.log(`Server is running at http//localhost:${PORT}`);
+    : console.log(`Server is running at http://localhost:${PORT}`);
 });
